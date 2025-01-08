0f682d9376c30484db1bf1d96ac1aa4f7e597f83
Shadow2907 <hieu29072004@gmail.com>
2025-01-08 23:33:19 +0700

    backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import authentication, userPersonalInfo, users, userFinancialInfo, userPersonalEvent, job
 from .configs.database import init_db
 import os, redis
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.on_event("startup")
 async def on_startup():
