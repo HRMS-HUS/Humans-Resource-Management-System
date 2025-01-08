949030eaadd031be8926653ea3d5e1c6831e534a
Shadow2907 <hieu29072004@gmail.com>
2025-01-08 23:42:11 +0700

    backend

--- backend/app/utils/email.py
+++ backend/app/utils/email.py
@@ -4,18 +4,6 @@ from dotenv import load_dotenv
 from typing import List
 from ..configs.email import config
 
-# load_dotenv()
-
-# config = ConnectionConfig(
-#     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-#     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-#     MAIL_FROM=os.getenv("MAIL_FROM"),
-#     MAIL_PORT=int(os.getenv("MAIL_PORT")),
-#     MAIL_SERVER=os.getenv("MAIL_SERVER"),
-#     MAIL_STARTTLS=True,
-#     MAIL_SSL_TLS=False,
-# )
-
 async def send_mail(subject: str, recipient: List[str], message: str):
     message = MessageSchema(
         subject = subject,
